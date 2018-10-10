0efc7e24236ee45c479625941a0a8ebf04360fa1
chloe alcorn <ca.alcorn@outlook.com>
2018-10-10 11:29:41 +0100

    other room added

--- src/main/java/RoomHire.java
+++ src/main/java/RoomHire.java
@@ -1,16 +1,30 @@
-//import java.lang.reflect.Array;
-//import java.util.ArrayList;
-//
-//public class RoomHire {
-//    private int capacity;
-//    private String type;
-//    private int dailyRate;
-//
-//
-//    public RoomHire(int capacity,String type, int dailyRate);
-//    this.capacity = capacity;
-//    this.type = type;
-//    this.dailyRate = dailyRate;
-//
-//
-//}
+import java.util.ArrayList;
+
+public class RoomHire {
+    private int capacity;
+    private String type;
+    private int dailyRate;
+    private ArrayList<Guest> guest;
+
+    public RoomHire(int capacity, String type , int dailyRate){
+        this.capacity = capacity;
+        this.type = type;
+        this.dailyRate = dailyRate;
+
+    }
+
+
+    public int hasCapacity() {
+        return capacity;
+    }
+
+    public String hasType() {
+        return type;
+    }
+
+    public int hasDailyRate() {
+        return dailyRate;
+    }
+
+
+}
--- src/test/java/RoomHireTest.java
+++ src/test/java/RoomHireTest.java
@@ -1,2 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class RoomHireTest {
+
+    RoomHire roomHire;
+    Guest guest;
+
+    @Before
+    public void before(){
+        roomHire = new RoomHire(3, "Meeting Room",15);
+        guest = new Guest();
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(3, roomHire.hasCapacity());
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals("Meeting Room", roomHire.hasType());
+    }
+
+    @Test
+    public void hasDailyRate(){
+        assertEquals(15 , roomHire.hasDailyRate());
+    }
 }
